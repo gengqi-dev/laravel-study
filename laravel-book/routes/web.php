77f8d2a110013a2461508193d398b7a10bfc0f60
GENG QI <geng-q@x1studio.co.jp>
2023-10-17 17:33:37 +0900

    laravel book

--- laravel-book/routes/web.php
+++ laravel-book/routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::namespace('App\Http\Controllers')->group(function () {
+    Route::get('/', 'TaskController@index');
 });
